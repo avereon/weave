4916bd3690b55c3e352007f849874d2c3f064740
Mark Soderquist <soderquistmv@ldschurch.org>
2017-11-01 10:05:33 -0600

    Continue work on tasks

--- source/main/java/com/xeomar/annex/UpdateTask.kt
+++ source/main/java/com/xeomar/annex/UpdateTask.kt
@@ -1,9 +1,29 @@
 package com.xeomar.annex
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 class UpdateTask(parameters: List<String>) : AnnexTask(parameters) {
 
+	fun needsElevation(): Boolean {
+		val target = Paths.get(getParameters()[1])
+
+		if( Files.isDirectory(target)) {
+			// have to test differently
+		}
+
+		return Files.exists(target) && !Files.isWritable(target)
+	}
+
+
 	override fun execute() {
+		val source = File(getParameters()[0])
+		val target = File(getParameters()[1])
+
 
 	}
+
 
 }
