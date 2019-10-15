bbf6e345152315b06a773599be831e975f32119a
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-10-15 09:24:43 -0600

    Improved unit test configuration and module definitions

--- source/main/java/module-info.java
+++ source/main/java/module-info.java
@@ -2,15 +2,11 @@ import com.avereon.zenna.Program;
 
 module com.avereon.zenna {
 
-	requires java.logging;
+	requires static java.logging;
 	requires javafx.controls;
 	requires com.avereon.zevra;
-	requires org.slf4j;
 
 	exports com.avereon.zenna;
-
-	// For testing
-	opens com.avereon.zenna.task;
 
 	provides com.avereon.product.Product with Program;
 
--- source/test/java/com/avereon/zenna/ProgramTest.java
+++ source/test/java/com/avereon/zenna/ProgramTest.java
@@ -5,13 +5,12 @@ import com.avereon.util.LogFlag;
 import com.avereon.util.LogUtil;
 import com.avereon.util.Parameters;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.io.*;
 
-import static java.time.Duration.ofSeconds;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 public class ProgramTest {
 
@@ -24,6 +23,7 @@ public class ProgramTest {
 	}
 
 	@Test
+	@Timeout( 2 )
 	public void testReadCommandsFromStdin() throws IOException {
 		InputStream originalInput = System.in;
 		PrintStream originalOutput = System.out;
@@ -34,33 +34,32 @@ public class ProgramTest {
 		System.setIn( new PipedInputStream( inputPipe ) );
 		System.setOut( new PrintStream( new PipedOutputStream( outputPipe ) ) );
 
-		assertTimeout( ofSeconds( 2 ), () -> {
-			try {
-				new Thread( () -> {
-					try {
-						new Program().start( UpdateFlag.STDIN, LogFlag.LOG_LEVEL, "none" );
-					} catch( Exception exception ) {
-						exception.printStackTrace( System.err );
-					}
-				} ).start();
+		try {
+			new Thread( () -> {
+				try {
+					new Program().start( UpdateFlag.STDIN, LogFlag.LOG_LEVEL, "none" );
+				} catch( Exception exception ) {
+					exception.printStackTrace( System.err );
+				}
+			} ).start();
 
-				inputPipe.write( "pause 130\n".getBytes( CHARSET ) );
-				inputPipe.write( "pause 170\n".getBytes( CHARSET ) );
-				inputPipe.close();
+			inputPipe.write( "pause 130\n".getBytes( CHARSET ) );
+			inputPipe.write( "pause 170\n".getBytes( CHARSET ) );
+			inputPipe.close();
 
-				String result1 = new BufferedReader( new InputStreamReader( outputPipe, CHARSET ) ).readLine();
-				assertThat( result1, is( "SUCCESS pause paused 130ms" ) );
-				String result2 = new BufferedReader( new InputStreamReader( outputPipe, CHARSET ) ).readLine();
-				assertThat( result2, is( "SUCCESS pause paused 170ms" ) );
-			} finally {
-				// Restore the original streams
-				System.setOut( originalOutput );
-				System.setIn( originalInput );
-			}
-		} );
+			String result1 = new BufferedReader( new InputStreamReader( outputPipe, CHARSET ) ).readLine();
+			assertThat( result1, is( "SUCCESS pause paused 130ms" ) );
+			String result2 = new BufferedReader( new InputStreamReader( outputPipe, CHARSET ) ).readLine();
+			assertThat( result2, is( "SUCCESS pause paused 170ms" ) );
+		} finally {
+			// Restore the original streams
+			System.setOut( originalOutput );
+			System.setIn( originalInput );
+		}
 	}
 
 	@Test
+	@Timeout( 2 )
 	public void testReadCommandsFromBytes() throws IOException {
 		PipedReader outputPipe = new PipedReader();
 		PipedWriter inputPipe = new PipedWriter();
@@ -68,26 +67,24 @@ public class ProgramTest {
 		PipedReader reader = new PipedReader( inputPipe );
 		PipedWriter writer = new PipedWriter( outputPipe );
 
-		assertTimeout( ofSeconds( 2 ), () -> {
-			new Thread( () -> {
-				try {
-					Program program = new Program();
-					LogUtil.configureLogging( program, Parameters.parse( LogFlag.LOG_LEVEL, "none" ) );
-					program.runTasksFromReader( reader, writer );
-				} catch( Exception exception ) {
-					exception.printStackTrace( System.err );
-				}
-			} ).start();
+		new Thread( () -> {
+			try {
+				Program program = new Program();
+				LogUtil.configureLogging( program, Parameters.parse( LogFlag.LOG_LEVEL, "none" ) );
+				program.runTasksFromReader( reader, writer );
+			} catch( Exception exception ) {
+				exception.printStackTrace( System.err );
+			}
+		} ).start();
 
-			inputPipe.write( "pause 170\n" );
-			inputPipe.write( "pause 190" );
-			inputPipe.close();
+		inputPipe.write( "pause 170\n" );
+		inputPipe.write( "pause 190" );
+		inputPipe.close();
 
-			String result1 = new BufferedReader( outputPipe ).readLine();
-			assertThat( result1, is( "SUCCESS pause paused 170ms" ) );
-			String result2 = new BufferedReader( outputPipe ).readLine();
-			assertThat( result2, is( "SUCCESS pause paused 190ms" ) );
-		} );
+		String result1 = new BufferedReader( outputPipe ).readLine();
+		assertThat( result1, is( "SUCCESS pause paused 170ms" ) );
+		String result2 = new BufferedReader( outputPipe ).readLine();
+		assertThat( result2, is( "SUCCESS pause paused 190ms" ) );
 	}
 
 }
