9dc8f3b4f40192eb056992d387e2c568a16c22a8
msoderquist <msoderquist@mastercontrol.com>
2020-11-20 12:38:40 -0700

    Add weave icon

---
+++ source/main/java/com/avereon/weave/icon/WeaveIcon.java
@@ -0,0 +1,53 @@
+package com.avereon.weave.icon;
+
+import com.avereon.zerra.image.SvgIcon;
+
+public class WeaveIcon extends SvgIcon {
+
+	private static final double C = 16;
+
+	private static final double SIZE = 4;
+
+	private static final double SPACE = 12;
+
+	public WeaveIcon() {
+
+		draw( weave1() );
+	}
+
+	public String weave1() {
+		StringBuilder builder = new StringBuilder();
+		builder.append( "M " + (C - (0.5 * SPACE)) + "," + C + " l -" + SIZE + ",0" );
+		builder.append( "M " + (C + (0.5 * SPACE)) + "," + C + " l " + SIZE + ",0" );
+		builder.append( "M " + (C - (0.5 * SIZE)) + "," + (C - (0.5 * (SPACE + SIZE))) + "l " + SIZE + ",0" );
+		builder.append( "M " + (C - (0.5 * SIZE)) + "," + (C + (0.5 * (SPACE + SIZE))) + "l " + SIZE + ",0" );
+
+		builder.append( "M " + C + "," + (C - (0.5 * SIZE)) + " l 0," + SIZE );
+		builder.append( "M " + (C - (0.5 * (SPACE + SIZE))) + "," + (C - (0.5 * SPACE) - SIZE) + " l 0," + SIZE );
+		builder.append( "M " + (C + (0.5 * (SPACE + SIZE))) + "," + (C - (0.5 * SPACE) - SIZE) + " l 0," + SIZE );
+		builder.append( "M " + (C - (0.5 * (SPACE + SIZE))) + "," + (C + (0.5 * SPACE)) + " l 0," + SIZE );
+		builder.append( "M " + (C + (0.5 * (SPACE + SIZE))) + "," + (C + (0.5 * SPACE)) + " l 0," + SIZE );
+
+		return builder.toString();
+	}
+
+	public String weave2() {
+		StringBuilder builder = new StringBuilder();
+		builder.append( "M " + C + "," + (C - (0.5 * SPACE)) + " l 0,-" + SIZE );
+		builder.append( "M " + C + "," + (C + (0.5 * SPACE)) + " l 0," + SIZE );
+		builder.append( "M " + (C - (0.5 * (SPACE + SIZE))) + "," + (C - (0.5 * SIZE)) + "l 0," + SIZE );
+		builder.append( "M " + (C + (0.5 * (SPACE + SIZE))) + "," + (C - (0.5 * SIZE)) + "l 0," + SIZE );
+
+		builder.append( "M " + (C - (0.5 * SIZE)) + "," + C + " l " + SIZE + ",0" );
+		builder.append( "M " + (C - (0.5 * SPACE) - SIZE) + "," + (C - (0.5 * (SPACE + SIZE))) + " l " + SIZE + ",0" );
+		builder.append( "M " + (C - (0.5 * SPACE) - SIZE) + "," + (C + (0.5 * (SPACE + SIZE))) + " l " + SIZE + ",0" );
+		builder.append( "M " + (C + (0.5 * SPACE)) + "," + (C - (0.5 * (SPACE + SIZE))) + " l " + SIZE + ",0" );
+		builder.append( "M " + (C + (0.5 * SPACE)) + "," + (C + (0.5 * (SPACE + SIZE))) + " l " + SIZE + ",0" );
+		return builder.toString();
+	}
+
+	public static void main( String[] parameters ) {
+		proof( new WeaveIcon() );
+	}
+
+}
--- source/main/java/module-info.java
+++ source/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.avereon.weave {
 	requires com.avereon.zenna;
 
 	exports com.avereon.weave;
+	exports com.avereon.weave.icon;
 
 	provides com.avereon.product.Product with com.avereon.weave.Program;
 
