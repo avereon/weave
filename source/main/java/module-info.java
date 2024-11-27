fac3dc105cc194532fcfdfb82b2eb784c108727f
msoderquist <msoderquist@mastercontrol.com>
2024-11-26 18:43:07 -0700

    Cleanup

--- source/main/java/module-info.java
+++ source/main/java/module-info.java
@@ -1,19 +1,16 @@
-import com.avereon.weave.Weave;
-
 module com.avereon.weave {
 
 	// Compile-time only
 	requires static lombok;
 
 	// Both compile-time and run-time
-	requires javafx.controls;
 	requires com.avereon.zevra;
 	requires com.avereon.zenna;
 	requires com.avereon.zarra;
+	requires javafx.controls;
 
 	exports com.avereon.weave;
 	exports com.avereon.weave.icon;
 
-	provides com.avereon.product.Product with Weave;
-
+	provides com.avereon.product.Product with com.avereon.weave.Weave;
 }
